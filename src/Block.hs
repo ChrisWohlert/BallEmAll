cda9eea911bcb7b0cd96352b501adfdc3ed5e567
ChrisWohlert <chriswohlert@hotmail.com>
2019-08-10 17:16:12 +0200

    Lavet replay system

--- src/Block.hs
+++ src/Block.hs
@@ -7,25 +7,25 @@ import Data.List
 import Debug.Trace
 import Ball
 
-getBlocks :: Float -> Float -> Float -> [Int] -> [Block]
-getBlocks width height floor (seed:seeds)= zipWith makePowerUpBlock
+getBlocks :: Int -> Float -> Float -> Float -> [Block]
+getBlocks seed width height floor = zipWith makePowerUpBlock
                                    (takeWhile isWithinWidth
                                       (iterate getNext (Block (Box 60 25) (50, height) Nothing))
                                       ++
                                       takeWhile isWithinFloor
-                                        (getBlocks width (height - 30) floor seeds))
-                                        (seed:seeds)
+                                        (getBlocks (seed * 32) width (height - 30) floor))
+                                        [seed ..]
   where
     getNext (Block (Box w h) (x, y) pu) = Block (Box w h) (x + w + 5, y) pu
     isWithinWidth (Block _ (x, _) _) = x < width
     isWithinFloor (Block _ (_, y) _) = y > floor
     makePowerUpBlock :: Block -> Int -> Block
     makePowerUpBlock (Block shape pos pu) i
-      | i `mod` 53 == 0 = trace (show (i `mod` 50)) Block shape pos $ Just $ BlockBallPowerUp makeNewFireBall
-      | i `mod` 351 == 0 = Block shape pos $ Just $ BlockBallPowerUp makeNewBalls
-      | i `mod` 35 == 0 = Block shape pos $ Just $ BlockBallPowerUp speedUp
-      | i `mod` 40 == 0 = Block shape pos $ Just $ BlockBatPowerUp widenBat
-      | otherwise = trace (show (i `mod` 50)) Block shape pos pu
+      | i `mod` 41 == 0 = Block shape pos $ Just $ BlockBallPowerUp makeNewFireBall
+      | i `mod` 54 == 0 = Block shape pos $ Just $ BlockBallPowerUp makeNewBalls
+      | i `mod` 38 == 0 = Block shape pos $ Just $ BlockBallPowerUp speedUp
+      | i `mod` 35 == 0 = Block shape pos $ Just $ BlockBatPowerUp widenBat
+      | otherwise = trace (show i) Block shape pos pu
         where
           makeNewFireBall (Ball shape pos vx vy bc batc wc (Just (FireBall r bs))) = updatePowerUp (Just (FireBall (maximum [r + 20, 50]) [])) (Ball shape pos vx vy bc batc wc (Just (FireBall r bs)))
           makeNewFireBall ball = updatePowerUp (Just (FireBall 50 [])) ball
@@ -33,8 +33,8 @@ getBlocks width height floor (seed:seeds)= zipWith makePowerUpBlock
           speedUp (Ball shape pos vx vy bc batc wc pu) = Ball shape pos (vx * 1.05) (vy * 1.05) bc batc wc pu
           widenBat (Bat (Box w h) pos d) = Bat (Box (w + 30) h) pos d
 
-destroyBlock :: Ball -> [Block] -> [Block]
-destroyBlock (Ball _ _ _ _ (Just (BlockCollision _ block)) _ _ (Just (MultiBall balls))) bs = foldl intersect bs $ map (`destroyBlock` bs) balls
-destroyBlock (Ball _ _ _ _ (Just (BlockCollision _ block)) _ _ (Just (FireBall _ bcs))) bs = bs \\ (block:bcs)
-destroyBlock (Ball _ _ _ _ (Just (BlockCollision _ block)) _ _ _) bs = bs \\ [block]
-destroyBlock _ bs = bs
+destroyBlock :: Ball -> [Block]
+destroyBlock (Ball _ _ _ _ (Just (BlockCollision _ block)) _ _ (Just (MultiBall balls))) = concatMap destroyBlock balls
+destroyBlock (Ball _ _ _ _ (Just (BlockCollision _ block)) _ _ (Just (FireBall _ bcs))) = block:bcs
+destroyBlock (Ball _ _ _ _ (Just (BlockCollision _ block)) _ _ _) = [block]
+destroyBlock _ = []
